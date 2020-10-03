e714477b3881e3c4496961e2273b5bd2d310ffaf
divya <divya.kumari@technogramsolutions.com>
2020-10-03 08:33:58 +0530

    minor change

--- Ques4.dart
+++ Ques4.dart
@@ -1,8 +1,9 @@
 void main() {
-  printPartyCategory(7, [2, 3, 4, 5, 5, 7, 77]);
+  List<String> arr= "2 3 4 5 5 7 77".split("");
+  printPartyCategory(7, arr);
 }
 
-void printPartyCategory(int i, List<int> list) {
+void printPartyCategory(int i, List<String> list) {
   if (list.toSet().length == list.length) print("GIRLS");
   else
   print("BOYS");
